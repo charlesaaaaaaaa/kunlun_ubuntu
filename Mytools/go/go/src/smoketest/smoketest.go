7ba571c3b89187188669fbf484e9d9e6cd2d870a
charlesaaaaaaaa <i1945725098@outlook.com>
2021-08-09 18:17:09 +0800

    3 times

--- Mytools/go/go/src/smoketest/smoketest.go
+++ Mytools/go/go/src/smoketest/smoketest.go
@@ -1,39 +1,42 @@
 package main
 
 import (
-  "database/sql"
-  "fmt"
-
-  _ "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+	"flag"
+	_ "github.com/lib/pq"
 )
 
 const (
-  HOST       = "localhost"
-  PORT       = 5401
-  USER       = "abc"
-  PASSWORD   = "abc"
-  DATABASE   = "postgres"
+	// Initialize connection constants.
+	//HOST     = "mydemoserver.postgres.database.azure.com"
+	//DATABASE = "postgres"
+	//USER     = "abc"
+	//PASSWORD = "abc"
 )
-func main() {
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
-  db, err := sql.Open("postgres", psqlInfo)
-  if err != nil {
-    panic(err)
-  }
-  defer db.Close()
 
-  err = db.Ping()
-  if err != nil {
-    panic(err)
-  }
-
-  fmt.Println("Successfully connected!")
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
+
 func main() {
+        var User string
+        var Pwd string
+        var Host string
+        var Port int
+	var Db string
+	flag.StringVar(&Host,"h","","默认为空")
+	flag.IntVar(&Port,"p",5001,"默认为5001")
+	flag.StringVar(&Pwd,"pwd","abc","默认为abc")
+	flag.StringVar(&Db,"d","postgres","默认为postgres")
+	flag.StringVar(&User,"u","abc","默认为abc")
+	flag.Parse()
+
+	fmt.Println("============= Golang ============")
 	// Initialize connection string.
-	var connectionString string = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require", HOST, PORT, USER, PASSWORD, DATABASE)
+	var connectionString string = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", Host, Port,User,Pwd,Db)
 
 	// Initialize connection object.
 	db, err := sql.Open("postgres", connectionString)
@@ -41,19 +44,39 @@ func main() {
 
 	err = db.Ping()
 	checkError(err)
-	fmt.Println("Successfully created connection to database")
+        fmt.Println("Successfully connection to database!!!")
 
 	// Drop previous table of same name if one exists.
-	_, err = db.Exec("drop table if exists smoketesttable_go;")
+	_, err = db.Exec("drop table if exists SmokeTestTable_go;")
 	checkError(err)
-	fmt.Println("step 1  :  drop table successful! ")
+	fmt.Println("Successfully drop   table")
 
 	// Create table.
-	_, err = db.Exec("create table smoketesttable_go(id int primary key,name text,gender text);")
+	_, err = db.Exec("create table SmokeTestTable_go(id int primary key,name text,gender text);")
 	checkError(err)
-	fmt.Println("step 2 : create table successful!")
+	fmt.Println("Successfully create table")
 
 	// Insert some data into table.
-	_, err = db.Exec("insert into smoketesttable_js1 values(1,'name1','male'),(2,'name2','female'),(3,'name3','male');")
-	fmt.Println("step 3 : insert table successful!")
+	sql_statement := "insert into SmokeTestTable_go values ($1, $2, $3);"
+	_, err = db.Exec(sql_statement, 1, "banana", "male")
+	checkError(err)
+	_, err = db.Exec(sql_statement, 2, "orange", "female")
+	checkError(err)
+	_, err = db.Exec(sql_statement, 3, "apple", "male")
+	checkError(err)
+	fmt.Println("Successfully insert table")
+
+	_, err = db.Exec("delete from Smoketesttable_csharp where id = 2")
+        checkError(err)
+        fmt.Println("Successfully delete table")
+
+        _, err = db.Exec("update SmokeTestTable_csharp set name = 'update' where id = 3")
+        checkError(err)
+        fmt.Println("Successfully update table")
+
+	_, err = db.Exec("select * from SmokeTestTable_csharp")
+        checkError(err)
+        fmt.Println("Successfully select table")
+	
+	fmt.Println("=================================")
 }
